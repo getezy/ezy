68ad39513884eaeba790670eeb1d944760b4c14e
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2022-04-27 19:35:31 +0300

    Init electron app

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+});
