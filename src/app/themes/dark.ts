ae1f52ee3457e9484c4e6e0b9990f79c124c236b
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2022-06-21 16:18:44 +0300

    Updated background color for dark theme

--- src/app/themes/dark.ts
+++ src/app/themes/dark.ts
@@ -2,4 +2,9 @@ import { createTheme } from '@nextui-org/react';
 
 export const darkTheme = createTheme({
   type: 'dark',
+  theme: {
+    colors: {
+      background: '#141414',
+    },
+  },
 });
