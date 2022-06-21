0137c92443c397a787f3b236e1b1282923b6d797
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2022-06-21 22:22:22 +0300

    Fixed tab bar item close bug

--- src/app/components/tabs/hooks/use-on-screen.tsx
+++ src/app/components/tabs/hooks/use-on-screen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export function useOnScreen(ref: any, rootMargin = '0px') {
+export function useOnScreen(ref: React.RefObject<HTMLDivElement>, rootMargin = '0px') {
   // State and setter for storing whether element is visible
   const [isIntersecting, setIntersecting] = React.useState(false);
+
   React.useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
@@ -13,12 +14,17 @@ export function useOnScreen(ref: any, rootMargin = '0px') {
         rootMargin,
       }
     );
+
     if (ref.current) {
       observer.observe(ref.current);
     }
+
     return () => {
-      observer.unobserve(ref.current);
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
     };
   }, []); // Empty array ensures that effect is only run on mount and unmount
+
   return isIntersecting;
 }
