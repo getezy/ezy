a3da352d7e4fc87bc8eaf5ff39c6cdecb1282aee
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2022-07-08 22:46:31 +0300

    Sync collections on startup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,22 @@
 import { NextUIProvider } from '@nextui-org/react';
 import React from 'react';
+import { useEffectOnce } from 'react-use';
 
 import { Main } from './pages';
-import { ThemeType, useSettingsStore } from './storage';
+import { ThemeType, useCollectionsStore, useSettingsStore } from './storage';
 import { darkTheme, globalStyles, lightTheme } from './themes';
 
 function App(): JSX.Element {
-  const theme = useSettingsStore((state) => state.type) === ThemeType.Dark ? darkTheme : lightTheme;
+  const theme = useSettingsStore((store) => store.type) === ThemeType.Dark ? darkTheme : lightTheme;
+  const { collections, updateCollection } = useCollectionsStore((store) => store);
 
   globalStyles();
+
+  useEffectOnce(() => {
+    collections.forEach((collection) => {
+      updateCollection(collection.id, collection);
+    });
+  });
 
   return (
     <NextUIProvider theme={theme}>
