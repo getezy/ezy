1f4dd10a9807d326c3952b25a7135a13b824da11
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2022-07-14 17:37:46 +0300

    Init storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
     'airbnb/hooks',
     'airbnb-typescript',
     'plugin:import/electron',
-    'prettier'
+    'prettier',
+    'plugin:storybook/recommended'
   ],
   env: {
     browser: true,
@@ -23,14 +24,24 @@ module.exports = {
     'prettier/prettier': ['error', {
       printWidth: 100,
       singleQuote: true,
-      trailingComma: 'es5',
+      trailingComma: 'es5'
     }],
 
     'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        'devDependencies': [
+          '**/*.stories.*',
+          '**/.storybook/**/*.*'
+        ],
+        'peerDependencies': true
+      }
+    ],
 
-    "react/prop-types": 'off',
+    'react/prop-types': 'off',
     'react/function-component-definition': 'off',
-    "react/require-default-props": "off",
+    'react/require-default-props': 'off',
     'react/jsx-props-no-spreading': 'off',
 
     'react-hooks/exhaustive-deps': 'off',
@@ -40,6 +51,6 @@ module.exports = {
       { 'groups': [['^\\u0000'], ['^[^.]'], ['^\\.'], ['^.+\\.s?css$']] }
     ],
 
-    'no-plusplus': 'off',
+    'no-plusplus': 'off'
   }
-}
+};
