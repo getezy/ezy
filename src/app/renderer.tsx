fbc4406401717bef6762dff672bb3c3b7e89ce75
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2022-08-02 14:51:29 +0300

    Updated actions and fixed file names

--- src/app/renderer.tsx
+++ src/app/renderer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import App from './App';
+import App from './app';
 
 const rootElement = document.getElementById('root');
 
