91f48ed4bd594333bec3f2a10757501f4afc0fc1
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2022-08-13 13:23:13 +0300

    Hide logs

--- src/app/pages/header.tsx
+++ src/app/pages/header.tsx
@@ -1,33 +1,34 @@
 import { faSquarePlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Spacer, styled } from '@nextui-org/react';
+import { Button, styled } from '@nextui-org/react';
 import React from 'react';
 
-import { useLogsStore } from '../storage';
+// import { useLogsStore } from '../storage';
 import { CreateCollectionModal } from './collections';
-import { LogsButton, LogsModal } from './logs';
+// import { LogsButton, LogsModal } from './logs';
 
 const HeaderWrapper = styled('div', {
   display: 'flex',
   flexWrap: 'nowrap',
+  justifyContent: 'center',
   alignItems: 'center',
   overflow: 'hidden',
 });
 
 export const Header: React.FC = () => {
   const [createCollectionModalVisible, setCreateCollectionModalVisible] = React.useState(false);
-  const [logsModalVisible, setLogsModalVisible] = React.useState(false);
-  const { newLogsAvailable, markAsReadLogs } = useLogsStore((store) => store);
+  // const [logsModalVisible, setLogsModalVisible] = React.useState(false);
+  // const { newLogsAvailable, markAsReadLogs } = useLogsStore((store) => store);
 
-  const handleLogsButtonClick = () => {
-    setLogsModalVisible(true);
-    markAsReadLogs();
-  };
+  // const handleLogsButtonClick = () => {
+  //   setLogsModalVisible(true);
+  //   markAsReadLogs();
+  // };
 
   return (
     <HeaderWrapper>
-      <LogsButton badgeVisible={newLogsAvailable} onClick={handleLogsButtonClick} />
-      <Spacer />
+      {/* <LogsButton badgeVisible={newLogsAvailable} onClick={handleLogsButtonClick} />
+      <Spacer /> */}
       <Button
         auto
         bordered
@@ -39,20 +40,19 @@ export const Header: React.FC = () => {
       >
         New collection
       </Button>
-      <Spacer />
       <CreateCollectionModal
         fullScreen
         closeButton
         open={createCollectionModalVisible}
         onClose={() => setCreateCollectionModalVisible(false)}
       />
-      <LogsModal
+      {/* <LogsModal
         open={logsModalVisible}
         closeButton
         fullScreen
         scroll
         onClose={() => setLogsModalVisible(false)}
-      />
+      /> */}
     </HeaderWrapper>
   );
 };
