1619c58bd4c242d19e7af40101825c86936d88bf
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2022-09-05 01:46:56 +0300

    chore: added version on status bar

--- src/app/pages/status-bar.tsx
+++ src/app/pages/status-bar.tsx
@@ -1,6 +1,7 @@
-import { Container } from '@nextui-org/react';
+import { Badge, Container } from '@nextui-org/react';
 import React from 'react';
 
+import packageJson from '../../../package.json';
 import { HorizontalLayoutIcon, VerticalLayoutIcon } from '../components';
 import { Alignment, useSettingsStore } from '../storage';
 
@@ -14,13 +15,19 @@ export const StatusBar: React.FC = () => {
   return (
     <Container
       gap={1}
-      fluid={false}
       responsive={false}
       display="flex"
+      wrap="nowrap"
+      direction="row"
       alignItems="center"
       css={{ borderTop: 'solid 1px $neutralBorder', height: 20 }}
     >
-      <Container gap={0} responsive={false} display="flex" justify="flex-end">
+      <Container gap={0} responsive={false} display="flex" justify="flex-start" alignItems="center">
+        <Badge size="xs" variant="flat" css={{ height: 18, fontSize: 8, userSelect: 'none' }}>
+          {packageJson.version}
+        </Badge>
+      </Container>
+      <Container gap={0} responsive={false} display="flex" justify="flex-end" alignItems="center">
         {alignment === Alignment.VERTICAL ? (
           <VerticalLayoutIcon onClick={() => handleAlignmentChange(Alignment.HORIZONTAL)} />
         ) : (
