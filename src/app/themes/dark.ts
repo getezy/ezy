b62530bee956b939dbc3ec6021749b602c857892
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2022-09-17 18:38:01 +0300

    style: change gradient color

--- src/app/themes/dark.ts
+++ src/app/themes/dark.ts
@@ -7,7 +7,7 @@ export const DarkTheme = createTheme({
       background: '#202225',
       backgroundContrast: '#292b2f',
       selection: 'rgba(166, 198, 224, 0.2)',
-      gradient: 'linear-gradient(112deg, $yellow300 -25%, $yellow500 30%, $cyan400 90%)',
+      gradient: 'linear-gradient(112deg, #acc917 -25%, #4f680f 90%)',
     },
   },
 });
--- src/app/themes/light.ts
+++ src/app/themes/light.ts
@@ -4,7 +4,7 @@ export const LightTheme = createTheme({
   type: 'light',
   theme: {
     colors: {
-      gradient: 'linear-gradient(112deg, $yellow400 -25%, $yellow600 30%, $cyan600 90%)',
+      gradient: 'linear-gradient(112deg, #acc917 -25%, #4f680f 90%)',
     },
   },
 });
